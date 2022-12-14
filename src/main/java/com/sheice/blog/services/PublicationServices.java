e4e7d5689f4bb447856705f6515dd8d837a4ec86
Sheice <75450007+Sheice@users.noreply.github.com>
2022-12-14 11:56:27 -0300

    create interface of publication and one method to create publication

---
+++ src/main/java/com/sheice/blog/services/PublicationServices.java
@@ -0,0 +1,9 @@
+package com.sheice.blog.services;
+
+import com.sheice.blog.dtos.PublicationDTO;
+
+public interface PublicationServices {
+
+    public PublicationDTO createPublication(PublicationDTO publicationDTO);
+
+}
