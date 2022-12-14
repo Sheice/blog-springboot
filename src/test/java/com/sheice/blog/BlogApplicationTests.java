34afc8c8fc4d5ebda148d5ec457ef41ceaf1a700
Sheice <75450007+Sheice@users.noreply.github.com>
2022-12-14 11:00:53 -0300

    create project

---
+++ src/test/java/com/sheice/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheice.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
